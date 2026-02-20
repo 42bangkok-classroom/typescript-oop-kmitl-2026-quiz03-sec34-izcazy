1367de759bcc085a1bd43f41a10877c70ca81895
izcazy <ice.izcazy@gmail.com>
2026-02-20 18:58:43 +0700

    testp04

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,35 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+    public courseName: string
+    public maxStudents: number
+    private enrolledStudents: number = 0
+    private isOpen: boolean = true
+
+    constructor(courseName: string, maxStudents: number){
+        this.courseName = courseName
+        this.maxStudents = maxStudents
+    }
+    enroll(): boolean{
+        return this.canEnroll()
+    }
+    closeCourse(): void{
+        this.isOpen = false
+    }
+    getAvailableSeats(): number{
+        return this.maxStudents
+    }
+    getCourseStatus(): string{
+        return this.isOpen ? 'Open' : 'Closed'
+    }
+    private canEnroll():boolean{
+        if(this.maxStudents > 0){
+            this.maxStudents--
+            return true
+        }
+        if(this.maxStudents < 1){
+            this.closeCourse
+        }
+        return false
+
+    }
+}
+
