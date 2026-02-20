92b9051664fb80ad9325a8a4ba96b23ffe5b177d
izcazy <ice.izcazy@gmail.com>
2026-02-20 17:51:45 +0700

    testp02

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,29 @@
-export class User {}
+export class User {
+    firstname: string
+    lastname: string
+    private age: number = 0
+    static  BIRTH_YEAR: number = 2000
+
+    constructor(){
+        this.firstname = ''
+        this.lastname = ''
+    }
+
+    setFirstname(){
+        return(`${this.firstname}`)
+    }
+    setLastname(){
+        return(`${this.lastname}`)
+    }
+    setAge(age: number){
+        if(age <= 0){
+            this.age = age
+        }
+    }
+    getAge(){
+        return this.age
+    }
+    getFullName(){
+        return(`${this.firstname} ${this.lastname}`)
+    }
+}
